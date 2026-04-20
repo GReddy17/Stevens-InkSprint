55faa73220b7aba854b04dd913dc2c17fe3a1022
ktunebe <ktunebe@gmail.com>
2026-04-20 12:09:44 -0500

    Added basic page/route framework for front end devs to start designing pages/components

---
+++ client/src/pages/ContestViewPage.jsx
@@ -0,0 +1,10 @@
+function ContestViewPage() {
+	return (
+		<div className="text-center py-16">
+			<h1 className="text-3xl font-bold mb-4">Contest View Page</h1>
+			<p className="text-gray-400">Replace with contest view page.</p>
+		</div>
+	)
+}
+
+export default ContestViewPage
