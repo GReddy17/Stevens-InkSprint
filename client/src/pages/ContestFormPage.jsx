eafc48e3a274a8be946eef616a3f467591eb8fbb
ktunebe <ktunebe@gmail.com>
2026-04-20 12:09:44 -0500

    Added basic page/route framework for front end devs to start designing pages/components

---
+++ client/src/pages/ContestFormPage.jsx
@@ -0,0 +1,10 @@
+function ContestFormPage() {
+	return (
+		<div className="text-center py-16">
+			<h1 className="text-3xl font-bold mb-4">Create Contest Page</h1>
+			<p className="text-gray-400">Replace with create contest page.</p>
+		</div>
+	)
+}
+
+export default ContestFormPage
---
+++ client/src/pages/NotFoundPage.jsx
@@ -0,0 +1,10 @@
+function NotFoundPage() {
+	return (
+		<div className="text-center py-16">
+			<h1 className="text-3xl font-bold mb-4">404</h1>
+			<p className="text-gray-400">Page not found.</p>
+		</div>
+	)
+}
+
+export default NotFoundPage
